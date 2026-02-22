32d72431a2009312cee4a768fb96e0055a0c1c1e
Wahedul Islam <wahedulislam.pro@gmail.com>
2026-02-23 00:13:38 +0600

    Add environment variable configuration and update server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import app from "./app";
+import { envVars } from "./config/env";
 
-const port = process.env.PORT || 5000;
 
 const bootstrap = () => {
     try {
-        app.listen(port, () => {
-            console.log(`Server is running on http://localhost:${port}`);
+        app.listen(envVars.PORT, () => {
+            console.log(`Server is running on http://localhost:${envVars.PORT}`);
         });
     } catch (error) {
         console.error("Error during server initialization:", error);
